77a829c647a5afd3f90c508bc8743cdc7e258bf7
YenLing1219 <cherry911219@gmail.com>
2023-05-01 01:49:08 +0800

    依標籤尋找書籍功能(home.html及book_search.html上方的搜尋欄) -搜尋條件包含書名、作者、科系、課程、老師皆可(使用sql的like語法) -小修html檔

---
+++ app.py
@@ -0,0 +1,166 @@
+from flask import Flask, render_template, request
+import pymysql
+
+app = Flask(__name__)
+
+
+#最初的模板
+"""
+@app.route('/')
+def hello_world():
+    return 'Hello World! 上架: /book_create    修改書籍資訊: /book_update/(+B_BookID)    書籍列表: /book_display    搜尋書籍: /book_search/(+search_str)'
+"""
+
+# 首頁
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+# 連接mysql
+def get_conn():
+    return pymysql.connect(
+        host = '127.0.0.1', 
+        user = 'root', 
+        password = '12345678', 
+        database = 'secondhand_bookweb', 
+        charset = 'utf8'
+    )
+
+# 新增資料
+def insert_or_update_data(sql):
+        conn = get_conn()
+        try:
+            cursor = conn.cursor()
+            cursor.execute(sql)
+            conn.commit()
+        finally:
+            conn.close()
+
+# [上架] 顯示網站
+@app.route('/book_create')
+def show_book_create():
+    return render_template("book_create.html")
+
+# [上架] 接收表單提交的數據
+@app.route('/do_book_create', methods=['POST'])
+def book_create():
+    print(request.form)
+    B_BookName = request.form.get("B_BookName")
+    B_ISBN = request.form.get("B_ISBN")
+    B_Author = request.form.get("B_Author")
+    B_BookVersion = request.form.get("B_BookVersion")
+    B_BookMajor = request.form.get("B_BookMajor")
+    B_LessonName = request.form.get("B_LessonName")
+    B_BookPic = request.form.get("B_BookPic") # 上傳圖片的功能還沒出來
+    B_BookStatus = request.form.get("B_BookStatus")
+    B_UsedStatus = request.form.get("B_UsedStatus")
+    B_UsedByTeacher = request.form.get("B_UsedByTeacher")
+    B_Extra_Info = request.form.get("B_Extra_Info")
+    B_Price = request.form.get("B_Price")
+    sql = f'''
+    insert into book_information(B_BookName, B_ISBN, B_Author, B_BookVersion, B_BookMajor, B_LessonName, B_BookPic, B_BookStatus, B_UsedStatus, B_UsedByTeacher, B_Extra_Info, B_Price)
+    values('{B_BookName}', '{B_ISBN}', '{B_Author}', '{B_BookVersion}', '{B_BookMajor}', '{B_LessonName}', '{B_BookPic}', {B_BookStatus}, '{B_UsedStatus}', '{B_UsedByTeacher}', '{B_Extra_Info}', {B_Price})
+    '''
+    print(sql)
+    insert_or_update_data(sql)
+    return "Book added successfully!"
+
+# [修改書籍資訊] 顯示網站
+@app.route('/book_update/<B_BookID>')
+def show_book_update(B_BookID):
+    sql = "select * from book_information where B_BookID=" + B_BookID
+    conn = get_conn()
+    try:
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(sql)
+        datas = cursor.fetchall()
+        book = datas[0]
+    finally:
+        conn.close()
+    return render_template("book_update.html", book = book)
+
+# [修改書籍資訊] 接收表單提交的數據
+@app.route('/do_book_update', methods=['POST']) #修改接值方式 將尾截掉.edit
+def book_update():
+    print(request.form)
+    B_BookID = request.form.get("B_BookID") #擺在下方接值.edit
+    B_BookName = request.form.get("B_BookName")
+    B_ISBN = request.form.get("B_ISBN")
+    B_Author = request.form.get("B_Author")
+    B_BookVersion = request.form.get("B_BookVersion")
+    B_BookMajor = request.form.get("B_BookMajor")
+    B_LessonName = request.form.get("B_LessonName")
+    B_BookPic = request.form.get("B_BookPic") # 上傳圖片的功能還沒出來
+    B_BookStatus = request.form.get("B_BookStatus")
+    B_UsedStatus = request.form.get("B_UsedStatus")
+    B_UsedByTeacher = request.form.get("B_UsedByTeacher")
+    B_Extra_Info = request.form.get("B_Extra_Info")
+    B_Price = request.form.get("B_Price")
+    sql = f'''
+    update book_information set B_BookName='{B_BookName}', B_ISBN='{B_ISBN}', B_Author='{B_Author}', B_BookVersion='{B_BookVersion}', 
+    B_BookMajor='{B_BookMajor}', B_LessonName='{B_LessonName}', B_BookPic='{B_BookPic}', B_BookStatus={B_BookStatus}, 
+    B_UsedStatus='{B_UsedStatus}', B_UsedByTeacher='{B_UsedByTeacher}', B_Extra_Info='{B_Extra_Info}', B_Price={B_Price}
+    where B_BookID={B_BookID}
+    '''
+    print(sql)
+    insert_or_update_data(sql)
+    return "Information updated successfully!"
+
+# 以下為新內容
+
+# [書籍列表] 顯示網站
+@app.route('/book_display')
+def show_book_display():
+    sql = "select B_BookID, B_BookName, B_BookPic, B_Price from book_information"
+    conn = get_conn()
+    try:
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(sql)
+        datas = cursor.fetchall()
+    finally:
+        conn.close()
+    return render_template("book_display.html", datas = datas)
+
+# [依"標籤"尋找書籍] 接收搜尋的數據
+@app.route('/do_book_search', methods=['GET'])
+def book_search():
+    search_str = request.args.get('search_str')
+    return show_book_search(search_str)
+
+# [依"標籤"尋找書籍] 顯示網站
+@app.route('/book_search/<search_str>')
+def show_book_search(search_str):
+    # 搜索欄位: 書名、作者、科系、課程、老師
+    sql = f'''select B_BookID, B_BookName, B_BookPic, B_Price from book_information where 
+    (B_BookName like '%{search_str}%') or (B_Author like '%{search_str}%') 
+    or (B_BookMajor like '%{search_str}%') or (B_LessonName like '%{search_str}%') 
+    or (B_UsedByTeacher like '%{search_str}%')'''
+    conn = get_conn()
+    try:
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(sql)
+        datas = cursor.fetchall()
+        print(datas)
+    finally:
+        conn.close()
+#    print(sql)
+    return render_template("book_search.html", datas = datas, search_str = search_str)
+
+
+# [查看書籍詳細資訊] 顯示網站
+@app.route('/book_detail/<B_BookID>')
+def show_book_detail(B_BookID):
+    sql = "select * from book_information where B_BookID=" + B_BookID
+    conn = get_conn()
+    try:
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(sql)
+        datas = cursor.fetchall()
+        book = datas[0]
+    finally:
+        conn.close()
+    return render_template("book_detail.html", book = book)
+
+# 執行
+if __name__ == '__main__': # 如果以主程式執行
+    app.run(debug=True) 
